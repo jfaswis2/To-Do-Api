096036e40ebe4ff91cb01142a04818a1b8ab4139
jfaswis2 <jfaswis2@gmail.com>
2022-12-08 19:41:12 +0100

    Find all tasks functionality has been created

--- src/main/java/com/example/todoapp/controller/TaskController.java
+++ src/main/java/com/example/todoapp/controller/TaskController.java
@@ -3,10 +3,9 @@ package com.example.todoapp.controller;
 import com.example.todoapp.persistence.entity.Task;
 import com.example.todoapp.service.TaskService;
 import com.example.todoapp.service.dto.TaskInDTO;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
@@ -20,6 +19,11 @@ public class TaskController {
     @PostMapping
     public Task createTask(@RequestBody TaskInDTO taskInDTO){
         return this.taskService.createTask(taskInDTO);
+    }
+
+    @GetMapping
+    public List<Task> findAll(){
+        return this.taskService.findAll();
     }
 
 }
--- src/main/java/com/example/todoapp/service/TaskService.java
+++ src/main/java/com/example/todoapp/service/TaskService.java
@@ -6,6 +6,8 @@ import com.example.todoapp.persistence.repository.TaskRepository;
 import com.example.todoapp.service.dto.TaskInDTO;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TaskService {
 
@@ -20,5 +22,9 @@ public class TaskService {
     public Task createTask(TaskInDTO taskInDTO){
         Task task = mapper.map(taskInDTO);
         return this.repository.save(task);
+    }
+
+    public List<Task> findAll(){
+        return this.repository.findAll();
     }
 }
