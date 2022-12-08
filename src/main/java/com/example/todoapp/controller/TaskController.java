4b8a195fbc329875663ceb9a15f031600130773a
jfaswis2 <jfaswis2@gmail.com>
2022-12-08 18:58:29 +0100

    PostMapping of CreateTask created

--- src/main/java/com/example/todoapp/controller/TaskController.java
+++ src/main/java/com/example/todoapp/controller/TaskController.java
@@ -1,7 +1,25 @@
 package com.example.todoapp.controller;
 
+import com.example.todoapp.persistence.entity.Task;
+import com.example.todoapp.service.TaskService;
+import com.example.todoapp.service.dto.TaskInDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
+
+    private final TaskService taskService;
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody TaskInDTO taskInDTO){
+        return this.taskService.createTask(taskInDTO);
+    }
+
 }
